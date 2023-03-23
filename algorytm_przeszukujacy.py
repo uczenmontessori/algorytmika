20eef11a1e734ba924178c5da633ab379cbd9ffb
uczenmontessori <127116352+uczenmontessori@users.noreply.github.com>
2023-03-23 22:14:42 +0100

    nadal nie wiem co zrobić
    nadal nie wiem dlaczego *zmienna licznik* ciągle pokazuje 1

--- algorytm_przeszukujacy.py
+++ algorytm_przeszukujacy.py
@@ -1,14 +1,13 @@
 import random
-a=[]
-for _ in range(200):
-    a.append(random.randint(1,200))
+a = []
+for _ in range(100):
+    a.append(random.randint(1,150))
 a.append(100)
 a = sorted(a)
 # print(a)
-licznik_1 = 0
 def wyszukiwanie():
     for element in a:
-        licznik_1 +=1 
+        licznik_1 = licznik_1 + 1
         if element == 100:
             print("W liście a jest", element)
             break
@@ -22,14 +21,22 @@ def divide(l):
     return l[:ix], l[ix:]
 
 
+licznik_1 = 0
 l1, l2 = divide(a)
 for x in range(len(a)):
-    licznik =+ 1
-    if l2[0] > 100:
-        l1, l2 = divide(l2)
+    n = x
+    if l1[0] == 100 or l2[0] == 100:
+        print("odnaleziono")
         break
-    if l1[len(l1)] > 100:
+    licznik_1 =+ 1
+    if l1[-1] >= 100:
         l1, l2 = divide(l1)
         break
+    else:
+        l1, l2 = divide(l2)
+        break
+
+#Nadal nie działa nie wiem co jeszcze zrobić
 print(licznik_1)
-#commit
+print(a)
+

4e9459f073cb7af88812ebc6c25acdbcdea47062
uczenmontessori <127116352+uczenmontessori@users.noreply.github.com>
2023-04-01 09:15:04 +0200

    Dodałem odpowiedź na pytanie.
    Poprawiłem trochę kod żeby był bardziej czytelny. licznik_1 jest równy 1 cały czas dlatego, że jest błąd w sposobie dodawania 1 za każdym razem kiedy uruchamiasz pętlę:
    licznik_1 =+ 1
    powinno być:
    licznik_1 += 1
    W pierwszej wersji po prostu przypisujesz 1 za każdym razem kiedy uruchamia się pętla.
    
    Dodatkowo dodałem kod:
        if len(l1) == 0 or len(l2) == 0:
            print("nie ma szukanej liczby")
            break
            
    który kończy pętlę w przypadku kiedy po podzieleniu listy na 2 zwraca jedną z list pustą a drugą z jednym elementem. Tak będzie się działo jeśli nie będzie szukanego elementu w liście. 
    
    Postaraj się poprawić logikę kodu, żeby dzialał. Jesteś już blisko.

--- algorytm_przeszukujacy.py
+++ algorytm_przeszukujacy.py
@@ -1,19 +1,24 @@
 import random
+
+szukana_liczba = 100
+
+
 a = []
 for _ in range(100):
     a.append(random.randint(1,150))
-a.append(100)
+a.append(szukana_liczba)
 a = sorted(a)
+
 # print(a)
-def wyszukiwanie():
-    for element in a:
-        licznik_1 = licznik_1 + 1
-        if element == 100:
-            print("W liście a jest", element)
-            break
-    else:
-        print('nie ma 100')
-    print('licznik_1', licznik_1)
+# def wyszukiwanie():
+#     for element in a:
+#         licznik_1 = licznik_1 + 1
+#         if element == szukana_liczba:
+#             print("W liście a jest", element)
+#             break
+#     else:
+#         print('nie ma szukana_liczba')
+#     print('licznik_1', licznik_1)
 
 
 def divide(l):
@@ -25,18 +30,23 @@ licznik_1 = 0
 l1, l2 = divide(a)
 for x in range(len(a)):
     n = x
-    if l1[0] == 100 or l2[0] == 100:
+    if len(l1) == 0 or len(l2) == 0:
+        print("nie ma szukanej liczby")
+        break
+
+    if l1[0] == szukana_liczba or l2[0] == szukana_liczba:
         print("odnaleziono")
         break
-    licznik_1 =+ 1
-    if l1[-1] >= 100:
+    licznik_1 += 1
+    if l1[-1] == szukana_liczba:
         l1, l2 = divide(l1)
         break
     else:
         l1, l2 = divide(l2)
-        break
 
-#Nadal nie działa nie wiem co jeszcze zrobić
+else:
+    print("nie ma szukanej liczby")
+
+
 print(licznik_1)
 print(a)
-

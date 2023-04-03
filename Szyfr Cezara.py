2759c21b7db1dd595c9b8c7eb57958a1447a95e9
uczenmontessori <127116352+uczenmontessori@users.noreply.github.com>
2023-04-03 15:52:11 +0200

    dodałem działający kod

--- Szyfr Cezara.py
+++ Szyfr Cezara.py
@@ -1,11 +1,12 @@
-#nadal nie wiem o co chodzi
-#...
 def szyfr(move,text):
     text2 = ''
     move2 = (move%26)
     for litera in text:
+        if move2 + ord(litera) > 122:
+            text2 = text2 + chr(ord(litera) + move2 - 26)
         # bład jest w linijce:
-        text2 = text2 + chr(ord(litera) + move2)
+        else:
+            text2 = text2 + chr(ord(litera) + move2)
         
     return text2
-print(szyfr(27,"za"))
+print(szyfr(1,"abcxyz"))
